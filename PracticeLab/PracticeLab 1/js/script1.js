9914f5edfa9283a1e8ad86bb32cdfd5f4c035aa8
Pliskin_ <albertlawrence.maglines.iics@ust.edu.ph>
2021-08-20 23:00:25 +0800

    init

--- PracticeLab/PracticeLab 1/js/script1.js
+++ PracticeLab/PracticeLab 1/js/script1.js
@@ -1,10 +1,13 @@
-const password1 = document.getElementById("pw1").value;
-const password2 = document.getElementById("pw2").value;
-const form = document.getElementById("loginForm").value;
 
-form.addEventListener('submit', (e) =>{
-    if (password1!=password2) {
-        alert("Password Mismatch")
-    } else {
-        alert("Password Matched!")
+function passwordConf{
+    let password1 = document.getElementById("pw1").value;
+    let password2 = document.getElementById("pw2").value;
+    const form = document.getElementById("loginForm").value;
+
+        form.addEventListener('submit', (e) =>{
+            if (password1===password2) {
+                 alert("Password Mismatch")
+            } else {
+                    alert("Password Matched!")
     }})
+}
--- Workspace/Module 1/js/script1.js
+++ Workspace/Module 1/js/script1.js
@@ -1,7 +1,9 @@
 function login(){
     let user = document.getElementById("user").value;
     let pwd = document.getElementById("pwd").value;
-    if(user === "admin" && pwd === "secret"){
+    
+    
+     if(user === "admin" && pwd === "secret"){
         alert("You have successfully signed in");
     }else{
         alert("Log-in failed");
