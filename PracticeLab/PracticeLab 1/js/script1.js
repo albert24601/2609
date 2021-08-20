b004b4a55a555aea5332875ae7be0222daa74849
Pliskin_ <albertlawrence.maglines.iics@ust.edu.ph>
2021-08-21 02:09:45 +0800

    init

--- PracticeLab/PracticeLab 1/js/script1.js
+++ PracticeLab/PracticeLab 1/js/script1.js
@@ -3,9 +3,8 @@ function passwordConf(){
     let password1 = document.getElementById("pw1").value;
     let password2 = document.getElementById("pw2").value;
 
-               if (password1===password2) {
-              alert("Password Mismatch");
+               if (password1!=password2) {
+              alert("Password Mismatch ");
                 } else {
-                alert("Password Matched!");
                     }
 }
