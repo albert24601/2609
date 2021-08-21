b9b4eccd9c5b941be79b66ccbb04d2de0a3cf64c
Pliskin_ <albertlawrence.maglines.iics@ust.edu.ph>
2021-08-22 02:09:30 +0800

    try

--- PracticeLab/PracticeLab 1/js/script1.js
+++ PracticeLab/PracticeLab 1/js/script1.js
@@ -1,10 +1,12 @@
 
-function passwordConf(){
-    let password1 = document.getElementById("pw1").value;
-    let password2 = document.getElementById("pw2").value;
+function login(){
+    let pw1 = document.getElementById("pw1").value;
+    let pw2 = document.getElementById("pw2").value;
+        if (pw1===pw2) {
+            alert("Test")
+        }else{
 
-               if (password1!=password2) {
-              alert("Password Mismatch ");
-                } else {
-                    }
-}
+        }
+
+
+    }
