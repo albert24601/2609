d330ad26dfceb7af9aa18925260ea1701a627351
Pliskin_ <albertlawrence.maglines.iics@ust.edu.ph>
2021-08-22 22:16:55 +0800

    repair

--- PracticeLab/PracticeLab 1/js/script1.js
+++ PracticeLab/PracticeLab 1/js/script1.js
@@ -1,12 +1,9 @@
-
-function login(){
-    let pw1 = document.getElementById("pw1").value;
+function login() {
+    let pw1 = document.getElementById("username").value;
     let pw2 = document.getElementById("pw2").value;
-        if (pw1===pw2) {
-            alert("Test")
-        }else{
-
-        }
-
+    if (pw1 === "admin") {
+        alert("Test");
+    } else {
 
     }
+}
